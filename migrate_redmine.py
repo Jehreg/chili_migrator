fbbfdd34dd2060842189b2f7fd512938ec75309d
Patrick Naubert <patrickn@xelerance.com>
2014-01-03 16:05:49 -0500

    Extend the virtual screen size to 200 columns

--- migrate_redmine.py
+++ migrate_redmine.py
@@ -17,111 +17,125 @@ def setup_tygerteam_commands():
 
 def setup_chili_commands():
   raw_commands = '''
+  stty cols 200
   vzctl enter 1003
   rm -rf /tmp/migration
   mkdir /tmp/migration
   chmod 777 /tmp/migration
   cd /tmp/migration
   su postgres
-  pg_dump --no-owner -a -t attachments                        redmine > attachments.sql
-  pg_dump --no-owner -a -t auth_sources                       redmine > auth_sources.sql
-  pg_dump --no-owner -a -t boards                             redmine > boards.sql
-  pg_dump --no-owner -a -t changes                            redmine > changes.sql
-  pg_dump --no-owner -a -t changesets                         redmine > changesets.sql
-  pg_dump --no-owner -a -t changesets_issues                  redmine > changesets_issues.sql
-  pg_dump --no-owner -a -t comments                           redmine > comments.sql
-  pg_dump --no-owner -a -t custom_fields                      redmine > custom_fields.sql
-  pg_dump --no-owner -a -t custom_fields_projects             redmine > custom_fields_projects.sql
-  pg_dump --no-owner -a -t custom_fields_trackers             redmine > custom_fields_trackers.sql
-  pg_dump --no-owner -a -t custom_values                      redmine > custom_values.sql
-  pg_dump --no-owner -a -t customers                          redmine > customers.sql
-  pg_dump --no-owner -a -t deliverables                       redmine > deliverables.sql
-  pg_dump --no-owner -a -t documents                          redmine > documents.sql
-  pg_dump --no-owner -a -t enabled_modules                    redmine > enabled_modules.sql
-  pg_dump --no-owner -a -t enumerations                       redmine > enumerations.sql
-  pg_dump --no-owner -a -t groups_users                       redmine > groups_users.sql
-  pg_dump --no-owner -a -t invoice_time_entries               redmine > invoice_time_entries.sql
-  pg_dump --no-owner -a -t invoices                           redmine > invoices.sql
-  pg_dump --no-owner -a -t issue_categories                   redmine > issue_categories.sql
-  pg_dump --no-owner -a -t issue_relations                    redmine > issue_relations.sql
-  pg_dump --no-owner -a -t issue_statuses                     redmine > issue_statuses.sql
-  pg_dump --no-owner -a -t issues                             redmine > issues.sql
-  pg_dump --no-owner -a -t journal_details                    redmine > journal_details.sql
-  pg_dump --no-owner -a -t journals                           redmine > journals.sql
-  pg_dump --no-owner -a -t kanban_issues                      redmine > kanban_issues.sql
-  pg_dump --no-owner -a -t member_roles                       redmine > member_roles.sql
-  pg_dump --no-owner -a -t members                            redmine > members.sql
-  pg_dump --no-owner -a -t messages                           redmine > messages.sql
-  pg_dump --no-owner -a -t news                               redmine > news.sql
-  pg_dump --no-owner -a -t open_id_authentication_associations redmine > open_id_authentication_associations.sql
-  pg_dump --no-owner -a -t open_id_authentication_nonces      redmine > open_id_authentication_nonces.sql
-  pg_dump --no-owner -a -t payments                           redmine > payments.sql
-  pg_dump --no-owner -a -t projects                           redmine > projects.sql
-  pg_dump --no-owner -a -t projects_trackers                  redmine > projects_trackers.sql
-  pg_dump --no-owner -a -t queries                            redmine > queries.sql
-  pg_dump --no-owner -a -t rates                            redmine > rates.sql
-  pg_dump --no-owner -a -t repositories                       redmine > repositories.sql
-  pg_dump --no-owner -a -t roles                              redmine > roles.sql
-  pg_dump --no-owner -a -t schema_migrations                  redmine > schema_migrations.sql
-  pg_dump --no-owner -a -t settings                           redmine > settings.sql
-  pg_dump --no-owner -a -t stuff_to_dos                       redmine > stuff_to_dos.sql
-  pg_dump --no-owner -a -t taggins                            redmine > taggings.sql
-  pg_dump --no-owner -a -t time_entries                       redmine > time_entries.sql
-  pg_dump --no-owner -a -t time_grid_issues_users             redmine > time_grid_issues_users.sql
-  pg_dump --no-owner -a -t timebanks                          redmine > timebanks.sql
-  pg_dump --no-owner -a -t tokens                             redmine > tokens.sql
-  pg_dump --no-owner -a -t trackers                           redmine > trackers.sql
-  pg_dump --no-owner -a -t user_preferences                   redmine > user_preferences.sql
-  pg_dump --no-owner -a -t users                              redmine > users.sql
-  pg_dump --no-owner -a -t versions                           redmine > versions.sql
-  pg_dump --no-owner -a -t watchers                           redmine > watchers.sql
-  pg_dump --no-owner -a -t wiki_content_versions              redmine > wiki_content_versions.sql
-  pg_dump --no-owner -a -t wiki_contents                      redmine > wiki_contents.sql
-  pg_dump --no-owner -a -t wiki_pages                         redmine > wiki_pages.sql
-  pg_dump --no-owner -a -t wiki_redirects                     redmine > wiki_redirects.sql
-  pg_dump --no-owner -a -t wikis                              redmine > wikis.sql
-  pg_dump --no-owner -a -t workflows              redmine > workflows.sql
+  pg_dump --no-owner -t attachments                        redmine > attachments.sql
+  pg_dump --no-owner -t auth_sources                       redmine > auth_sources.sql
+  pg_dump --no-owner -t boards                             redmine > boards.sql
+  pg_dump --no-owner -t changes                            redmine > changes.sql
+  pg_dump --no-owner -t changesets                         redmine > changesets.sql
+  pg_dump --no-owner -t changesets_issues                  redmine > changesets_issues.sql
+  pg_dump --no-owner -t comments                           redmine > comments.sql
+  pg_dump --no-owner -t custom_fields                      redmine > custom_fields.sql
+  pg_dump --no-owner -t custom_fields_projects             redmine > custom_fields_projects.sql
+  pg_dump --no-owner -t custom_fields_trackers             redmine > custom_fields_trackers.sql
+  pg_dump --no-owner -t custom_values                      redmine > custom_values.sql
+  pg_dump --no-owner -t customers                          redmine > customers.sql
+  pg_dump --no-owner -t deliverables                       redmine > deliverables.sql
+  pg_dump --no-owner -t documents                          redmine > documents.sql
+  pg_dump --no-owner -t enabled_modules                    redmine > enabled_modules.sql
+  pg_dump --no-owner -t enumerations                       redmine > enumerations.sql
+  pg_dump --no-owner -t groups_users                       redmine > groups_users.sql
+  pg_dump --no-owner -t invoice_time_entries               redmine > invoice_time_entries.sql
+  pg_dump --no-owner -t invoices                           redmine > invoices.sql
+  pg_dump --no-owner -t issue_categories                   redmine > issue_categories.sql
+  pg_dump --no-owner -t issue_relations                    redmine > issue_relations.sql
+  pg_dump --no-owner -t issue_statuses                     redmine > issue_statuses.sql
+  pg_dump --no-owner -t issues                             redmine > issues.sql
+  pg_dump --no-owner -t journal_details                    redmine > journal_details.sql
+  pg_dump --no-owner -t journals                           redmine > journals.sql
+  pg_dump --no-owner -t kanban_issues                      redmine > kanban_issues.sql
+  pg_dump --no-owner -t member_roles                       redmine > member_roles.sql
+  pg_dump --no-owner -t members                            redmine > members.sql
+  pg_dump --no-owner -t messages                           redmine > messages.sql
+  pg_dump --no-owner -t news                               redmine > news.sql
+  pg_dump --no-owner -t open_id_authentication_associations redmine > open_id_authentication_associations.sql
+  pg_dump --no-owner -t open_id_authentication_nonces      redmine > open_id_authentication_nonces.sql
+  pg_dump --no-owner -t payments                           redmine > payments.sql
+  pg_dump --no-owner -t projects                           redmine > projects.sql
+  pg_dump --no-owner -t projects_trackers                  redmine > projects_trackers.sql
+  pg_dump --no-owner -t queries                            redmine > queries.sql
+  pg_dump --no-owner -t rates                            redmine > rates.sql
+  pg_dump --no-owner -t repositories                       redmine > repositories.sql
+  pg_dump --no-owner -t roles                              redmine > roles.sql
+  pg_dump --no-owner -t schema_migrations                  redmine > schema_migrations.sql
+  pg_dump --no-owner -t settings                           redmine > settings.sql
+  pg_dump --no-owner -t stuff_to_dos                       redmine > stuff_to_dos.sql
+  pg_dump --no-owner -t taggings                            redmine > taggings.sql
+  pg_dump --no-owner -t time_entries                       redmine > time_entries.sql
+  pg_dump --no-owner -t time_grid_issues_users             redmine > time_grid_issues_users.sql
+  pg_dump --no-owner -t timebanks                          redmine > timebanks.sql
+  pg_dump --no-owner -t tokens                             redmine > tokens.sql
+  pg_dump --no-owner -t trackers                           redmine > trackers.sql
+  pg_dump --no-owner -t user_preferences                   redmine > user_preferences.sql
+  pg_dump --no-owner -t users                              redmine > users.sql
+  pg_dump --no-owner -t versions                           redmine > versions.sql
+  pg_dump --no-owner -t watchers                           redmine > watchers.sql
+  pg_dump --no-owner -t wiki_content_versions              redmine > wiki_content_versions.sql
+  pg_dump --no-owner -t wiki_contents                      redmine > wiki_contents.sql
+  pg_dump --no-owner -t wiki_pages                         redmine > wiki_pages.sql
+  pg_dump --no-owner -t wiki_redirects                     redmine > wiki_redirects.sql
+  pg_dump --no-owner -t wikis                              redmine > wikis.sql
+  pg_dump --no-owner -t workflows              redmine > workflows.sql
   exit  # Exit the postgres account
   exit  # Exit the container
-  scp /var/lib/vz/private/1003/tmp/migration/*.sql root@www.tygerteam.com:/tmp/redmine-tables/
-  scp /var/lib/vz/private/1003/var/www/rails_apps/chiliproject/files/* root@www.tygerteam.com:/tmp/redmine-files/
+  # Blank line
+  rsync -aP /var/lib/vz/private/1003/tmp/migration/*.sql root@www.tygerteam.com:/tmp/redmine-tables/
+  rsync -aP /var/lib/vz/private/1003/var/www/rails_apps/chiliproject/files/* root@www.tygerteam.com:/tmp/redmine-files/
   '''
   commands = [i for i in raw_commands.split("\n")]
   return commands
 
 
-def setup_redmine_commands():
+def setup_vzhost_commands():
   raw_commands = '''
-  CTID=6016
-  CTNAME=redmine
-  vzctl create $CTID --hostname $CTNAME.xelerance.com --name $CTNAME --ostemplate ubunta-12.04-i386
-  vzctl set $CTID --ipadd 192.168.88.60 --privvmpages unlimited --save
-  vzctl start $CTID
-  vzctl enter $CTID
+  stty cols 200
+  vzctl stop 6016
+  vzctl destroy 6016
+  vzctl create 6016 --hostname redmine2.xelerance.com --name redmine2 --ostemplate ubuntu-12.04-i386 --config basic
+  vzctl set 6016 --ipadd 192.168.88.60 --privvmpages unlimited --save
+  vzctl start 6016
+  sleep 10
+  vzctl exec 6016 apt-get update
+  vzctl exec 6016 apt-get install -y openssh-server
+  '''
+  commands = [i for i in raw_commands.split("\n")]
+  return commands
+
+def setup_redmine_commands():
+  raw_commands  = '''
+  stty cols 200
   echo 'Acquire::http::Proxy "http://192.168.88.24:3142/apt-cacher/";' > /etc/apt/apt.conf.d/01proxy
   apt-get update
   apt-get upgrade
-  apt-get install postgresql
-  apt-get install --no-install-recommends git rubygems ruby1.9.1-dev ruby-rmagick rake make gcc openssl
-  apt-get install ruby-fastercsv libapache2-mod-passenger imagemagick libxml2-dev libxslt-dev logrotate ssl-cert
+  apt-get install -y postgresql
+  apt-get install -y --no-install-recommends git rubygems ruby1.9.1-dev ruby-rmagick rake make gcc openssl
+  apt-get install -y libapache2-mod-passenger imagemagick libxml2-dev libxslt1-dev logrotate ssl-cert
   exit
-  scp root@www.tygerteam.com:/tmp/redmine-tables/* /VEs/private/$CTID/tmp/migration/
-  scp root@www.tygerteam.com:/tmp/redmine-files/* /VEs/private/$CTID/var/lib/redmine/default/files/
-  vzctl enter $CTID
+  rsync -aP root@www.tygerteam.com:/tmp/redmine-tables/* /VEs/private/6016/tmp/migration/
+  vzctl enter 6016
   REDMINE_PASSWORD=$(openssl rand -base64 33)
+  echo $REMINE_PASSWORD > /tmp/password
   cd /tmp/migration
   su postgres
   createuser redmine --no-superuser --no-createdb --no-createrole --login --encrypted
   psql << EOM
-  alter user redmine PASSWORD '$REDMINE_PASSWORD';
+  alter user redmine PASSWORD '`cat /tmp/password`';
   \q
   EOM
   createdb --owner=redmine --encoding=utf-8 -T template0 redmine
-  for x in `ls *.sql` ; do psql redmine -c \"delete from ${x%.sql}\" ; done
+  for x in `ls *.sql` ; do psql redmine -c "delete from ${x%.sql}" ; done
+  exit   # Get out of the postgres account
   sed -ibk \"s/custom_filter/filter/\" auth_sources.sql
   sed -ibk \"s/created_at/created_on/\" issues.sql
   sed -ibk \"s/journaled_id/journalized_id/\" journals.sql
   sed -ibk \"s/lock_version/version/\" wiki_contents.sql
+  su postgres
   for x in `ls *.sql` ; do psql redmine < $x 1>> /tmp/output.txt 2>&1; done
   exit  # Get out of Postgres user
   cd /var/www
